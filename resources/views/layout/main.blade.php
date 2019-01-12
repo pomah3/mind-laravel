0fa33d80a6b7134cff4c0968a2796851d75b3e33
Semenov Roman <roman.semenov@protonmail.com>
2019-01-12 14:14:16 +0300

    icon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ App::getLocale() }}">
 <head>
-    <meta charset="UTF-8">
     <title>@yield('title')</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+
 </head>
 <body>
     @yield('body')

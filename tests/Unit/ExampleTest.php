3221866c0eb6bfb1cda2fbaf7985d75cf3f322bd
Semenov Roman <roman.semenov@protonmail.com>
2019-01-13 14:47:23 +0300

    dusk

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,17 +3,140 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Excel\Formatter;
 
-class ExampleTest extends TestCase
-{
+final class FormatterTest extends TestCase {
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @dataProvider providerNames
      */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
+    public function testName(string $name, string $need): void {
+        $this->assertEquals(
+            $need,
+            Formatter::name($name)
+        );
+    }
+
+    public function providerNames(): array {
+        $right = "Семенов";
+        $variants = [
+            $right,
+            " семенов  ",
+            " семеноВ",
+            "  семёнов  ",
+        ];
+
+        $ret = [];
+        foreach ($variants as $v) {
+            $ret[] = [$v, $right];
+        }
+        return $ret;
+    }
+
+    /**
+     * @dataProvider providerAbbrNames
+     */
+    public function testAbbrName(string $name, string $need): void {
+        $this->assertEquals(
+            $need,
+            Formatter::abbr_name($name)
+        );
+    }
+
+    public function providerAbbrNames(): array {
+        $right = "Семенов Р.С.";
+        $variants = [
+            $right,
+            "семенов р.  с.",
+            "Семёнов Р С",
+            "  сЕМЕНОВ Р. С  ",
+            "семенов р.с."
+        ];
+
+        $ret = [];
+        foreach ($variants as $v) {
+            $ret[] = [$v, $right];
+        }
+        return $ret;
+    }
+
+    /**
+     * @dataProvider providerGroups
+     */
+    public function testGroup(string $group, string $need): void {
+        $this->assertEquals(
+            $need,
+            Formatter::group($group)
+        );
+    }
+
+    public function providerGroups(): array {
+        $right = "10-4";
+        $variants = [
+            $right,
+            "10.4",
+            "  10  . 4   ",
+            "Класс 10 4.",
+            "10 - 4",
+            "10--4",
+            "10 . 4",
+            "10. 4",
+            "10 4"
+        ];
+
+        $ret = [];
+        foreach ($variants as $v) {
+            $ret[] = [$v, $right];
+        }
+        return $ret;
+    }
+
+    /**
+     * @dataProvider providerDays
+     */
+    public function testDay(string $day, string $need): void {
+        $this->assertEquals(
+            $need,
+            Formatter::day($day)
+        );
+    }
+
+    public function providerDays(): array {
+        $right = "Monday";
+        $variants = [
+            "понедельник",
+            "   пОнЕдЕльник "
+        ];
+
+        $ret = [];
+        foreach ($variants as $v) {
+            $ret[] = [$v, $right];
+        }
+        return $ret;
+    }
+
+    /**
+     * @dataProvider providerTimes
+     */
+    public function testTime(string $time, string $need): void {
+        $this->assertEquals(
+            $need,
+            Formatter::time($time)
+        );
+    }
+
+    public function providerTimes(): array {
+        $right = "10:59";
+        $variants = [
+            $right,
+            "10.59",
+            "   10   59 "
+        ];
+
+        $ret = [];
+        foreach ($variants as $v) {
+            $ret[] = [$v, $right];
+        }
+        return $ret;
     }
 }
+

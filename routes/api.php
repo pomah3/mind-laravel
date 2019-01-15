bf94c18370897653c2182242d2d216ed4839817c
Semenov Roman <roman.semenov@protonmail.com>
2019-01-15 17:43:54 +0300

    add api methods

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,55 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\User;
+use App\transaction;
+use Illuminate\Support\Facades\DB;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware("api_token")->group(function() {
+
+    Route::prefix("/users")->group(function() {
+        Route::get('', function() {
+            return User::all();
+        });
+
+        Route::get('{user}', function (User $user) {
+            return $user;
+        });
+
+        Route::get("/check/{user}/{password}", function (User $user, $password) {
+            return [
+                "data" => $user->password == $password
+            ];
+        });
+    });
+
+    Route::prefix("/transactions")->group(function() {
+        Route::get('', function() {
+            return Transaction::all();
+        });
+        Route::get('{tr}', function(Transaction $tr) {
+            return $tr;
+        });
+        Route::get('of_user/{user}', function(User $user) {
+            return Transaction::of_student($user);
+        });
+        Route::post("/add", function(Request $request) {
+            //todo
+        });
+    });
+
+    Route::prefix("/groups")->group(function() {
+        Route::get("", function() {
+            return DB::table("roles")
+                ->select('role_arg as name')
+                ->where('role', 'student')
+                ->groupBy('role_arg')
+                ->orderBy('role_arg')
+                ->get();
+        });
+    });
 });
+
+

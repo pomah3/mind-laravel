604c501036a7ba389fef1c3f730de6a563df6a6b
Semenov Roman <roman.semenov@protonmail.com>
2019-02-05 14:38:46 +0300

    add profile info

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,41 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    private function get_daytime() {
+        $h = (new \DateTime())->format("H");
+        $h = intval($h);
+
+        $night   = "Доброй ночи" ;
+        $morning = "Доброе утро" ;
+        $day     = "Добрый день" ;
+        $evening = "Добрый вечер";
+
+        if ($h < 5)
+            return $night;
+        elseif ($h < 12)
+            return $morning;
+        elseif ($h < 16)
+            return $day;
+        elseif ($h < 22)
+            return $evening;
+
+        return $night;
+    }
+
+    private function get_timetable_date() {
+        $h = (new \DateTime())->format("H");
+        $h = intval($h);
+
+        if ($h < 16)
+            return \Carbon\Carbon::now();
+        else
+            return new \Carbon\Carbon("tomorrow");
+    }
+
     public function index() {
-        return view("profile");
+        return view("profile", [
+            "daytime" => $this->get_daytime(),
+            "date" => $this->get_timetable_date()
+        ]);
     }
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,18 +3,53 @@
 @php($user = Auth::user())
 
 @section('title')
-    {{ __("profile.title") }}
+    Профиль
 @endsection
 
 @section('content')
-    <div class="info">
-        {{ __("user.show.name") }}: {{ $user->get_name() }} <br>
-        @if ($user->type === "student")
-            {{ __('user.show.balance') }}: {{ App\Transaction::get_balance($user) }}<br>
-            {{ __('user.show.group') }}: {{ $user->student()->get_group() }}<br>
+    <div class="block-information profile-information">
+        <h2>{{ $daytime }}, @user(["user"=>$user])</h2>
+        @if ($user->has_role("student"))
+            <h2>
+                Баланс: <strong>
+                    {{ $user->student()->get_balance() }}
+                </strong>
+            </h2>
+            <h3>
+                Класс: <strong>
+                    {{ $user->student()->get_group() }}
+                </strong>
+            </h3>
+            <h3>
+                Классный руководитель: <strong>
+                    @user(["user" => $user->student()->get_classruk()])
+                </strong>
+            </h3>
         @endif
     </div>
-    <hr>
+    <div class="block-information timetable">
+        <h2>
+            {{ Carbon\Carbon::now()->format("l") == $date->format("l") ? "Сегодня" : "Завтра"}}:
+            <strong>{{ $date->format("l") }}</strong>,
+            {{ $date->format("d.m.Y") }}
+        </h2>
+        @if (isset($timetable))
+            <table class="timetable-table">
+                <tr>
+                    <th></th>
+                    <th>Предмет</th>
+                    <th>Каб.</th>
+                </tr>
+                @foreach ($timetable as $lesson)
+                    <tr class="{{ $lesson->is_now ? "lesson-now" : ""}}">
+                        <td>{{ $lesson->number  }}</td>
+                        <td>{{ $lesson->lesson  }}</td>
+                        <td>{{ $lesson->cabinet }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        @endif
+    </div>
     <div class="notifications">
         @foreach ($user->notifications as $n)
             @component($n->data['view'], [

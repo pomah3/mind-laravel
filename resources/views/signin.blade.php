09591c41b98379a3d0dd3ee27d1fe58e8b3827bd
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-02-07 21:36:46 +0300

    signin page design

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -1,53 +1,64 @@
-@extends("layout.main")
+<!DOCTYPE html>
+<html lang="{{ App::getLocale() }}" class="signin-html">
+<head>
+    <title>@yield('title')</title>
 
-@section('title')
-    {{ __("signin.title") }}
-@endsection
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <link rel="stylesheet" href="{{ URL::asset("css/lib.css") }}">
+    <link rel="stylesheet" href="{{ URL::asset("css/app.css") }}">
 
-@section("body")
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+</head>
+<body class="signin-body">
 
-    <form action="/signin" method="POST">
-        @csrf
-        <input required
-            type="text"
-            name="login"
-            value="{{ old("login") }}"
-        >
-        <input required
-            type="password"
-            name="password"
-        >
-        <input type="submit">
-    </form>
+    <div class="signin-card">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-    <h1>Via edu tatar:</h1>
-    <form action="/signin/edu" method="POST">
-        @csrf
-        <input required
-            type="text"
-            name="login"
-            value="{{ old("login") }}"
-        >
-        <input required
-            type="password"
-            name="password"
-        >
-        <input type="submit">
-    </form>
+        <form action="/signin" method="POST">
+            @csrf
+            <div class="input-group form-group">
+                <div class="input-group-prepend">
+                    <i class="fa fa-user input-group-text" aria-hidden="true"></i>
+                </div>
+                <input required
+                    type="text"
+                    name="login"
+                    class="form-control"
+                    placeholder="Логин" 
+                    value="{{ old("login") }}"
+                >
+            </div>
+            <div class="input-group form-group">
+                <div class="input-group-prepend">
+                    <i class="fa fa-lock input-group-text" aria-hidden="true"></i>
+                </div>
+                <input required
+                    type="password"
+                    name="password"
+                    class="form-control"
+                    placeholder="Пароль" 
+                >
+            </div>
+            <input type="submit" class="submit" value="Войти">
+            <div class="check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Вход через edu.tatar</label>
+            </div>
+        </form>
+    </div>
 
-<i class="fas fa-space-shuttle fa-rotate"></i>
-<i class="fas fa-space-shuttle fa-rotate-90"></i><br>
-<i class="fas fa-space-shuttle fa-rotate-270"></i>
-<i class="fas fa-space-shuttle fa-rotate-180"></i>
+    <script src="{{ URL::asset("js/app.js") }}"></script>
+    @stack("scripts")
 
-
-@endsection
+</body>
+</html>

a8bf1ca19c34363f386cb7aa7015777d3731e130
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-02-07 22:38:01 +0300

    menu design

--- resources/views/layout/logined.blade.php
+++ resources/views/layout/logined.blade.php
@@ -1,12 +1,26 @@
-@extends('layout.main')
 
-@section('body')
+<!DOCTYPE html>
+<html lang="{{ App::getLocale() }}">
+<head>
+    <title>@yield('title')</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <link rel="stylesheet" href="{{ URL::asset("css/lib.css") }}">
+    <link rel="stylesheet" href="{{ URL::asset("css/app.css") }}">
+    <script>window.userId = null</script>
+</head>
+<body>
+    
+    <div class="main_container">
     <script>window.userId = {{ Auth::user()->id }}</script>
 
     <header>
         <div class="top-menu">
             <div class="container-menu">
-                <img src="/img/logo_full.png" width="200px">
+                <img src="/img/logo_animation.gif">
             </div>
         </div>
 
@@ -18,4 +32,10 @@
 
     @yield('content')
 
-@endsection
+    </div>
+
+    <script src="{{ URL::asset("js/app.js") }}"></script>
+    @stack("scripts")
+
+</body>
+</html>

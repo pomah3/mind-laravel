faac054c5fa042d9c8269a0613b8750a0b661c53
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-02-08 17:42:41 +0300

    points page design

--- resources/views/notifications/points_got.blade.php
+++ resources/views/notifications/points_got.blade.php
@@ -9,19 +9,19 @@
         {{ $from_id }}, {{ $time }}
     </div>
     @if ($points > 0)
-        <div class="cause">
-            {{ $cause }}
-        </div>
         <span class="points good-points">
             +{{ $points }}
         </span>
-    @else
         <div class="cause">
             {{ $cause }}
         </div>
+    @else
         <span class="points bad-points">
             {{ $points }}
         </span>
+        <div class="cause">
+            {{ $cause }}
+        </div>
     @endif
     {{-- {{ var_dump($notification->data) }} --}}
 </div>
--- resources/views/points/show.blade.php
+++ resources/views/points/show.blade.php
@@ -7,31 +7,60 @@
 @endsection
 
 @section('content')
-    <h1>{{ $student->get_name() }}</h1>
-
-    Баланс: {{ $student->student()->get_balance() }}
-
-    <table>
-        <tr>
-            <th>{{ __("points.show.time") }}</th>
-            <th>{{ __("points.show.points") }}</th>
-            <th>{{ __("points.show.from") }}</th>
-            <th>{{ __("points.show.to") }}</th>
-            <th>{{ __("points.show.cause") }}</th>
-        </tr>
-
+    <div class="container container-points">
+        <h2>Баланс: <strong>{{ $student->student()->get_balance() }}</strong> баллов</h2>
         @foreach ($transactions as $tr)
-            <tr>
-                <td>{{ $tr->created_at }}</td>
-                <td>{{ $tr->points }}</td>
-                @if ($tr->from_user)
-                    <td>@user(["user"=>$tr->from_user])</td>
-                @else
-                    <td></td>
-                @endif
-                <td>@user(["user"=>$tr->to_user])</td>
-                <td>{{ $tr->cause->title }}</td>
-            </tr>
+            <div class="day">
+                <h3 class="date">
+                    
+                </h3>
+                <div class="transaction">
+                    <div class="from-name">
+                        
+                        <span class="transaction-time">
+                            {{ $tr->created_at }}
+                        </span>
+                    </div>
+                    @if ($tr->points > 0)
+                        <span class="points good-points">
+                            +{{ $tr->points }}
+                        </span>
+                        <div class="cause">
+                            {{ $tr->cause->title }}
+                        </div>
+                    @else
+                        <span class="points bad-points">
+                            {{ $tr->points }}
+                        </span>
+                        <div class="cause">
+                            {{ $tr->cause->title }}
+                        </div>
+                    @endif
+                </div>
+            </div>
+            {{-- <table>
+                <tr>
+                    <th>{{ __("points.show.time") }}</th>
+                    <th>{{ __("points.show.points") }}</th>
+                    <th>{{ __("points.show.from") }}</th>
+                    <th>{{ __("points.show.to") }}</th>
+                    <th>{{ __("points.show.cause") }}</th>
+                </tr> --}}
+
+              {{--   @foreach ($transactions as $tr)
+                    <tr>
+                        <td>{{ $tr->created_at }}</td>
+                        <td>{{ $tr->points }}</td>
+                        @if ($tr->from_user)
+                            <td>@user(["user"=>$tr->from_user])</td>
+                        @else
+                            <td></td>
+                        @endif
+                        <td>@user(["user"=>$tr->to_user])</td>
+                        <td>{{ $tr->cause->title }}</td>
+                    </tr>
+                @endforeach 
+            </table>--}}
         @endforeach
-    </table>
+    </div>
 @endsection

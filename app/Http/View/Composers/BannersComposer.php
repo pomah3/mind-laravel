6c7b8ebc83dff7c458a2111a6d92241abe1456fc
Semenov Roman <roman.semenov@protonmail.com>
2019-02-14 15:00:45 +0300

    banner fixes

--- app/Http/View/Composers/BannersComposer.php
+++ app/Http/View/Composers/BannersComposer.php
@@ -4,9 +4,15 @@ namespace App\Http\View\Composers;
 
 use Illuminate\View\View;
 use App\Banner;
+use Carbon\Carbon;
 
 class BannersComposer {
     public function compose(View $view) {
-        $view->with("banners", Banner::all());
+        $view->with(
+            "banners",
+            Banner::where("from_date", "<=", Carbon::now())
+                  ->where("till_date", ">=", Carbon::now())
+                  ->get()
+        );
     }
 }

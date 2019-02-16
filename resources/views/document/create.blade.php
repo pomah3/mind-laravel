cd68ea4c97cedf53602db959475cdf41cd7f07c7
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-02-16 08:54:32 +0300

    document page design

--- resources/views/document/create.blade.php
+++ resources/views/document/create.blade.php
@@ -7,26 +7,34 @@
 @endsection
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    <div class="container container-points">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-    <form action="/documents" method="POST" enctype="multipart/form-data">
-        @csrf
+        <form action="/documents" method="POST" enctype="multipart/form-data" class="form-50">
+            @csrf
 
-        <input type="text" name="title" placeholder="title"><br>
-        <input type="text" name="access" placeholder="access"><br>
+            
+            <input type="text" name="title" placeholder="title" class="form-control">
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="access" id="exampleRadios1" value="option1" checked>
+                    <label class="form-check-label" for="exampleRadios1">
+                        Всем
+                    </label>
+            </div>
+            <input type="radio" name="access" class="form-control" value='["all"]'>
 
-        <input type="file" name="file"><br>
+            <input type="file" name="file" class="form-control">
 
-        <input type="submit">
+            <input type="submit" class="submit">
 
-    </form>
-
+        </form>
+    </div>
 @endsection
--- resources/views/document/index.blade.php
+++ resources/views/document/index.blade.php
@@ -7,25 +7,32 @@
 @endsection
 
 @section('content')
-    @can('create', App\Document::class)
-        <a href="/documents/create">создать</a>
-    @endcan
+    <div class="container container-points">
+        <h2>Документы</h2>
+        @can('create', App\Document::class)
+            <a href="/documents/create" class="add-banner">+</a>
+        @endcan
 
-    <ul>
-        @foreach ($documents as $document)
-            <li>
-                <a href="/documents/{{ $document->id }}">
-                    {{ $document->title }}
-                </a>
-                @can('update', $document)
-                    (<a href="/documents/{{ $document->id }}/edit">edit</a>)
-                @endcan
-                @can('delete', $document)
-                    <button class="remove-doc" doc-id="{{ $document->id }}">&times;</button>
-                @endcan
-            </li>
-        @endforeach
-    </ul>
+        <ul>
+            @forelse ($documents as $document)
+                <li>
+                    <a href="/documents/{{ $document->id }}">
+                        {{ $document->title }}
+                    </a>
+                    @can('update', $document)
+                        (<a href="/documents/{{ $document->id }}/edit">edit</a>)
+                    @endcan
+                    @can('delete', $document)
+                        <button class="remove-doc" doc-id="{{ $document->id }}">&times;</button>
+                    @endcan
+                </li>
+            @empty
+                <div class="not-found">
+                    Нет доступных документов
+                </div>
+            @endforelse
+        </ul>
+    </div>
 
     @push('scripts')
         <script>

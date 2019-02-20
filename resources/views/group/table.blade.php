938b2e373583ed00a0e264ed236542103d193814
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-02-20 22:21:34 +0300

    group page design

--- resources/views/group/table.blade.php
+++ resources/views/group/table.blade.php
@@ -1,21 +1,21 @@
-<div>
-    Класс: {{ $group }}
-    Баланс: {{ $balance }} баллов
+<div class="w-50 b-10 one-class {{-- вот сюда надо будет вставить --}}">
+    <h2>Класс: <strong>{{ $group }}</strong></h2>
+    <h3>Баланс: <strong>{{ $balance }}</strong> баллов</h3>
+    <table class="table table-sm">
+        @foreach($users as $user)
+            <tr>
+                <td>
+                    @user(["user" => $user])
+                </td>
+
+                <td>
+                    {{ $user->student()->get_balance() }}
+                </td>
+
+                <td>
+                    <a href="/points/{{ $user->id }}" class="a-designed a-small">Подробнее</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
 </div>
-<table>
-    @foreach($users as $user)
-        <tr>
-            <td>
-                @user(["user" => $user])
-            </td>
-
-            <td>
-                {{ $user->student()->get_balance() }}
-            </td>
-
-            <td>
-                <a href="/points/{{ $user->id }}">Подробнее</a>
-            </td>
-        </tr>
-    @endforeach
-</table>

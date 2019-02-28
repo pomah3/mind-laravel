5ad1003cf645a2c08d406edf2b2d6f4384de407a
Semenov Roman <roman.semenov@protonmail.com>
2019-02-28 21:10:06 +0300

    separate factories

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Question::class, function($faker) {
+    if ($faker->boolean()) {
+        return [
+            "asker_id" => User::all()->random()->id,
+            "question" => $faker->text
+        ];
+    }
+
+    return [
+        "asker_id" => User::all()->random()->id,
+        "question" => $faker->text,
+        "answerer_id" => User::all()->random()->id,
+        "answer" => $faker->text,
+        "answered_at" => $faker->dateTime(),
+        "created_at" => $faker->dateTime()
+    ];
+});

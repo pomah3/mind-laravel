a1bb18ef6eb9baf4eefee74313ba6e6653147c55
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-03-05 17:30:37 +0300

    added translations and translations to events page

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -6,39 +6,40 @@
 
 @section('content')
     <div class="container container-points">
-        <h2>Мероприятия</h2>
+        <h2>{{ __('event.index.title') }}</h2>
         @can('create', App\Event::class)
             <a href="/events/create" class="add-event">+</a>
         @endcan
 
         @forelse ($events as $event)
-            <div class="event">
-                <h1>{{ $event->title }}</h1>
-                <button class="event-remove" event-id="{{ $event->id }}">remove</button>
-                author: @user(["user" => $event->author]) <br>
-                from_date: {{ $event->from_date }} <br>
-                till_date: {{ $event->till_date }} <br>
+            <div class="one-event">
+                <button class="event-delete" event-id="{{ $event->id }}">&times;</button>
+                <h2><strong>{{ $event->title }}</strong></h2>
                 <div class="event-description">
                     {{ $event->description }}
                 </div>
+                <div class="banner-label">{{ __('event.about.author') }}: @user(["user" => $event->author])</div>
+                <div class="banner-label">{{ __('event.about.from_date') }}: <span>{{ $event->from_date }}</span></div>
+                <div class="banner-label">{{ __('event.about.till_date') }}: <span>{{ $event->till_date }}</span></div>
 
-                <ul>
+                <div>{{ __('event.about.partisipants') }}:</div>
+                <ol class="event-partisipants">
                     @foreach ($event->users as $user)
                         <li>
                             @user(["user"=>$user])
                         </li>
                     @endforeach
-                </ul>
+                </ol>
             </div>
         @empty
             <div class="not-found">
-                Нет активных мероприятий
+                {{ __('event.not-found') }}
             </div>
         @endforelse
     </div>
     @push('scripts')
         <script>
-            $(".event-remove").click(function() {
+            $(".event-delete").click(function() {
                 let that = this;
                 let id = $(that).attr("event-id");
                 $.ajax({

f216061b7547df44064c94dccbb072a82082d852
Semenov Roman <roman.semenov@protonmail.com>
2019-03-06 16:35:27 +0300

    add socped to status policy

--- app/Policies/StatusPolicy.php
+++ app/Policies/StatusPolicy.php
@@ -4,23 +4,21 @@ namespace App\Policies;
 
 use App\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Roles;
 
 class StatusPolicy
 {
     use HandlesAuthorization;
 
     public function seeIndex(User $user) {
-        return $user->has_role("teacher");
+        return $user->has_role(Roles::SOCPED);
     }
 
     public function see(User $user, User $of_user) {
-        return (
-                $user->has_role("classruk") &&
-                $user->get_role_arg("classruk") == $of_user->get_role_arg("student")
-            ) || $user->id == $of_user->id;
+        return $user->has_role(Roles::SOCPED);
     }
 
     public function set(User $user, User $of_user) {
-        return $this->see($user, $of_user);
+        return $user->has_role(Roles::SOCPED);
     }
 }

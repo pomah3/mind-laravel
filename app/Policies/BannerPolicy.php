02ea314e0eb9096f48ca43d46dd20e1c3fa69681
Semenov Roman <roman.semenov@protonmail.com>
2019-03-12 21:07:35 +0300

    edit banner policy

--- app/Policies/BannerPolicy.php
+++ app/Policies/BannerPolicy.php
@@ -5,55 +5,28 @@ namespace App\Policies;
 use App\User;
 use App\Banner;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Roles;
 
-class BannerPolicy
-{
+class BannerPolicy {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the banner.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Banner  $banner
-     * @return mixed
-     */
-    public function view(User $user)
-    {
-        return $user->has_role("moderator");
+    public function view(User $user) {
+        return $user->has_role(Roles::MODERATOR)
+            || $user->has_role(Roles::PEDORG);
     }
 
-    /**
-     * Determine whether the user can create banners.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        return $user->has_role("moderator");
+    public function create(User $user) {
+        return $user->has_role(Roles::MODERATOR)
+            || $user->has_role(Roles::PEDORG);
     }
 
-    /**
-     * Determine whether the user can update the banner.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Banner  $banner
-     * @return mixed
-     */
-    public function update(User $user)
-    {
-        return $user->has_role("moderator");
+    public function update(User $user) {
+        return $user->has_role(Roles::MODERATOR)
+            || $user->has_role(Roles::PEDORG);
     }
 
-    /**
-     * Determine whether the user can delete the banner.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Banner  $banner
-     * @return mixed
-     */
-    public function delete(User $user)
-    {
-        return $user->has_role("moderator");
+    public function delete(User $user) {
+        return $user->has_role(Roles::MODERATOR)
+            || $user->has_role(Roles::PEDORG);
     }
 }

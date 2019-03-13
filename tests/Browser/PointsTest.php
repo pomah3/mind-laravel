6ef4989eebe5b9c7ff238781d84f14cd6dbb4b48
Semenov Roman <roman.semenov@protonmail.com>
2019-03-13 19:01:09 +0300

    fix

--- tests/Browser/PointsTest.php
+++ tests/Browser/PointsTest.php
@@ -37,7 +37,7 @@ class PointsTest extends DuskTestCase {
                 ->visit('/points/add')
                 ->pause(1000);
 
-            dd($browser_t->element("form")->getAttribute('innerHTML'));
+            dd($browser_t->element("body")->getAttribute('innerHTML'));
 
                 // ->select("#select-group", $group)
                 // ->pause(1000)

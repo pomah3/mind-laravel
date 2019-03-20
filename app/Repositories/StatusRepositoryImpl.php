35aca2080695c19debb61350d520d2b66ee83754
Semenov Roman <roman.semenov@protonmail.com>
2019-03-20 22:46:46 +0300

    change status saving

--- app/Repositories/StatusRepositoryImpl.php
+++ app/Repositories/StatusRepositoryImpl.php
@@ -20,7 +20,9 @@ class StatusRepositoryImpl implements StatusRepository {
         $data = [];
 
         foreach ($this->get_all_statuses() as $status) {
-            $data[$status] = DB::table("statuses")->where("title", $status)->count();
+            $data[$status] = DB::table("statuses")
+                               ->where("title", $status)
+                               ->count();
         }
 
         $a = DB::select("select count(id) as count from users where id not in (select user_id from statuses)");
@@ -31,12 +33,15 @@ class StatusRepositoryImpl implements StatusRepository {
 
     public function get_status(User $user): Status {
         $status = Status::where("user_id", $user->id)
+                        ->where("updated_at", ">=", now()->startOfDay())
+                        ->where("updated_at", "<=", now()->endOfDay())
                         ->first();
 
         if ($status)
             return $status;
 
         $status = new Status;
+        $status->user_id = $user->id;
         $status->title = $this->get_unknown_status();
 
         return $status;
@@ -44,6 +49,14 @@ class StatusRepositoryImpl implements StatusRepository {
 
     public function set_status_title(User $user, string $title) {
         $status = $this->get_status($user);
+
+        $need_new = $status->title == $this->get_unknown_status();
+
+        if ($need_new) {
+            $status = new Status;
+            $status->user_id = $user->id;
+        }
+
         $status->title = $title;
         $status->save();
     }

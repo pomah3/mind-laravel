0492e955bf87a95c77ef05380ea88da3d02d34d6
Semenov Roman <roman.semenov@protonmail.com>
2019-04-04 12:14:32 +0300

    add word template for clinic scenario

--- app/Scenarios/Need_to_get_to_clinic_Scenario.php
+++ app/Scenarios/Need_to_get_to_clinic_Scenario.php
@@ -2,11 +2,14 @@
 
 namespace App\Scenarios;
 
+use App\Document;
 use App\Scenarios\Fields\DateInputField;
 use App\Scenarios\Fields\TextInputField;
 use App\Scenarios\Fields\UserInputField;
 use App\User;
+use App\Utils;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 
 class Need_to_get_to_clinic_Scenario extends Scenario {
     public function get_title() {
@@ -17,7 +20,7 @@ class Need_to_get_to_clinic_Scenario extends Scenario {
 		return [
 			"init" => $this->init_stage(),
 			"find_vospit" => $this->find_vospit_stage(),
-			"show_to_student" => $this->show_to_student_stage()
+			"show" => $this->show_stage()
 		];
 	}
 
@@ -45,8 +48,12 @@ class Need_to_get_to_clinic_Scenario extends Scenario {
 			],
 			function($sc, $input, $user) {
 				$sc->set_data("vospit", $input[0]->get_user()->id);
-				$sc->set_users([User::find($sc->get_data("student"))]);
-				$sc->set_stage("show_to_student");
+                $sc->set_data("zam", $user->id);
+
+				$sc->set_users([User::find($sc->get_data("student")), $user]);
+				$sc->set_stage("show");
+
+                $sc->prepare_order();
 			},
 			function($sc, $user) {
 				return view("scenario.list.Need_to_get_to_clinic.find_vospit", [
@@ -57,14 +64,49 @@ class Need_to_get_to_clinic_Scenario extends Scenario {
 		);
 	}
 
-	public function show_to_student_stage() {
+    public function prepare_order() {
+        $student = User::find($this->get_data("student"));
+        $vospit = User::find($this->get_data("vospit"));
+        $zam = User::find($this->get_data("zam"));
+        $date = new \Carbon\Carbon($this->get_data("when"));
+
+        $doc = Utils::word_template("word/need_to_clinic.docx", [
+            "student" => $student->get_name(),
+            "group" => $student->student()->get_group(),
+            "zam" => $zam->get_name(),
+            "vospit" => $vospit->get_name(),
+            "date" => (string) $date
+        ]);
+
+        $this->set_data("doc_id", $doc->id);
+    }
+
+	public function show_stage() {
 		return new FinalStage(
 			function($sc, $user) {
-				return view("scenario.list.Need_to_get_to_clinic.show_to_student", [
-                    "vospit" => User::find($sc->get_data("vospit")),
-                    "date" => new Carbon($sc->get_data("when"))
-                ]);
+                if ($user->type == "student")
+                    return $sc->show_to_student();
+                else
+                    return $sc->show_to_zam();
 			}
 		);
 	}
+
+    private function show_to_student() {
+        return view("scenario.list.Need_to_get_to_clinic.show_to_student", [
+            "vospit" => User::find($this->get_data("vospit")),
+            "date" => new Carbon($this->get_data("when"))
+        ]);
+    }
+
+    private function show_to_zam() {
+        return view("scenario.list.Need_to_get_to_clinic.show_to_zam", [
+            "student" => User::find($this->get_data("student")),
+            "vospit" => User::find($this->get_data("vospit")),
+            "date" => new Carbon($this->get_data("when")),
+            "doc" => URL::signedRoute("documents.show", [
+                "document" => Document::find($this->get_data("doc_id"))
+            ])
+        ]);
+    }
 }

252c2c24e4fcbc2767cd0c8cf9b3d303b19c36cc
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-04-06 21:09:14 +0300

    added user agreement

--- resources/views/first_visit.blade.php
+++ resources/views/first_visit.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ App::getLocale() }}">
 <head>
     <meta charset="UTF-8">
-    <title>Mind</title>
+    <title>Пользовательское соглашение</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="/img/favicon.ico">
@@ -16,7 +16,7 @@
             <div class="jumbotron">
                 {!! $text !!}
                 <p class="lead">
-                <a class="btn btn-primary btn-lg" href="/" role="button">Я согласен</a>
+                <a class="btn btn-primary btn-lg" href="/" role="button">Я соглашаюсь с правилами использования Сервиса</a>
                 </p>
             </div>
         </div>

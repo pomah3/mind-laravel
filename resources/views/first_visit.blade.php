4c186b5538f09b023e867886b39c76431628bdb4
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-04-06 18:54:27 +0300

    first_visit page design

--- resources/views/first_visit.blade.php
+++ resources/views/first_visit.blade.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ App::getLocale() }}">
 <head>
     <meta charset="UTF-8">
     <title>Mind</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <link rel="stylesheet" href="{{ mix("css/lib.css") }}">
+    <link rel="stylesheet" href="{{ mix("css/app.css") }}">
 </head>
 <body>
-    {!! $text !!}
 
-    <a href="/">Я согласен</a>
+    <div class="first-visit">
+        <div class="container">
+            <div class="jumbotron">
+                {!! $text !!}
+                <p class="lead">
+                <a class="btn btn-primary btn-lg" href="/" role="button">Я согласен</a>
+                </p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>

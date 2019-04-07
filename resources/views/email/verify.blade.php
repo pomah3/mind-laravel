e4eb6674453ccd1b3d0881ea5bf891de7ad1fe1e
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2019-04-07 19:13:40 +0300

    emails design

--- resources/views/email/verify.blade.php
+++ resources/views/email/verify.blade.php
@@ -1,6 +1,48 @@
 @extends("email.base")
 
 @section("content")
-    {{ $user->get_name() }}, подтвердите свою почту
-    Ссылка: <a href="{{ URL::signedRoute("verify_email", ["user"=>$user]) }}">клик</a>
+
+<table align="center" width="100%" height="270px" cellpadding="20" cellspacing="0" style="margin: 0 auto; text-align: center; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; background-position: 50% 69%; background-size: cover; max-width: 600px;" background="http://mind-itl-kfu.ru/img/white_pattern_for_email.png">
+    <tr>
+        <td align="center">
+            <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td align="center" style="font-size: 48px;">
+                        <b>Подтвердите почту!</b>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<table align="center" width="100%" cellpadding="20" cellspacing="0" style="margin: 0 auto; text-align: center; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; background-color: #ffffff; color: #000000; max-width: 600px;">
+    <tr>
+        <td align="center">
+            <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td align="left" style="font-size: 20px;">
+                        {{ $user->get_name() }}, подтвердите свою почту по <a href="{{ URL::signedRoute("verify_email", ["user"=>$user]) }}">ссылке</a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<table align="center" width="100%" cellpadding="20" cellspacing="0" style="margin: 0 auto; text-align: center; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%; background-color: #ffffff; color: #000000; max-width: 600px;">
+    <tr>
+        <td align="center">
+            <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td align="left" style="font-size: 25px;">
+                        Приятного пользования! <br>
+                        <b>Команда Mind</b>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
 @endsection

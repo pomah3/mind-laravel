0a6f4dc9bd66ad9a59ff7ca55af8675305d034ea
Semenov Roman <roman.semenov@protonmail.com>
2019-04-09 18:54:58 +0300

    add update command

---
+++ app/Console/Commands/Update.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
+
+class Update extends Command {
+    protected $signature = 'mind:update {branch=master}';
+    protected $description = 'Do some thing to update';
+
+    public function handle() {
+        $branch = $this->argument('branch');
+
+        system("git checkout $branch");
+        system("git pull");
+
+        system("composer install");
+
+        Artisan::call("cache:clear");
+        Cache::flush();
+
+        Artisan::call("migrate");
+        Artisan::call("config:cache");
+        Artisan::call("view:cache");
+
+        system("npm i");
+    }
+}

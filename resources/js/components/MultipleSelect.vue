54958e5500ab4a5455957e8291e3e5260c2019da
Semenov Roman <roman.semenov@protonmail.com>
2019-04-14 12:00:37 +0300

    add multiple select component

---
+++ resources/js/components/MultipleSelect.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div
+            v-for="(select, id) in selected"
+        >
+            <input type="hidden" :name="name + '[]'" :value="select.value">
+            <div>
+                {{ select.name }}
+                <span v-on:click="remove(id)">
+                    &times;
+                </span>
+            </div>
+        </div>
+        <select ref="select">
+            <option
+                v-for="variant in variants"
+                :value="variant.value"
+            >
+                {{ variant.name }}
+            </option>
+        </select>
+        <div v-on:click="add()">Добавить</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["variants", "name"],
+        data: function() {
+            return {
+                selected: []
+            };
+        },
+        methods: {
+            add: function() {
+                let select = this.$refs.select;
+                let {value, selectedIndex} = select;
+                let name = select[selectedIndex].innerText.trim();
+                this.selected.push({name, value});
+            },
+            remove: function(id) {
+                this.selected.splice(id, 1);
+            }
+        }
+    }
+</script>
